00b63dfd341641fb1120ef9df5d309f7dd807fae
Andrei Filomatov <filomatov@gmail.com>
2026-07-14 20:52:06 +0300

    completed homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    if (a === 0) {
+        alert('Первый коэффициент "a" не должен быть равен нулю. Введите заново новое значение');
+        return;
+    }
+    
+    let discriminant = b ** 2 - 4 * a * c;
+    let result = [];
+
+    if (discriminant > 0) {
+        result[0] = (-b - discriminant ** (1 / 2)) / (2 * a);
+        result[1] = (-b + discriminant ** (1 / 2)) / (2 * a); 
+    } else if (discriminant === 0) {
+        result[0] = -b / (2 * a);
+    }
+    return result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    let percentMonth = percent / (100 * 12);
+    let bodyCredit = amount - contribution;
+    let monthPayment = bodyCredit * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+    let sumPayment = (monthPayment * countMonths).toFixed(2);
+    return sumPayment;
 }
