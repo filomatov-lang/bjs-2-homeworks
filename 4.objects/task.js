bd1707f661f9479216a800feb51c57369e50c7b1
Andrei Filomatov <filomatov@gmail.com>
2026-07-22 00:53:38 +0300

    Полностью решено задание  4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,24 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  this?.marks === undefined ? undefined : this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+  return this?.marks === undefined || this.marks.length === 0  ? 0 : this.marks.reduce((sum, assessment, index, marks) => sum + assessment / marks.length, 0);
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
